af5f719a76649ba86ad98add51c71a3f14d9cd8c
Eric Thomas <ewolft@gmail.com>
2020-10-20 19:46:05 -0400

    Fix min_value for money donations

--- donations/forms.py
+++ donations/forms.py
@@ -3,7 +3,7 @@ from django import forms
 from django.core.exceptions import ValidationError
 
 class MoneyDonationForm(forms.Form):
-    money_total = forms.DecimalField(label='Money Total (USD)', max_digits=8, decimal_places=2, min_value=0)
+    money_total = forms.DecimalField(label='Money Total (USD)', max_digits=8, decimal_places=2, min_value=0.01)
 
 class TimeDonationForm(forms.Form):
     time_total = forms.DurationField(label='Time Total (s)')
