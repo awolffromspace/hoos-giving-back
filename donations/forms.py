00f1eff8a4df5bf3f641c6104af1c48b7811d8ed
Eric Thomas <ewolft@gmail.com>
2020-10-20 19:13:33 -0400

    Add unit tests for forms

--- donations/forms.py
+++ donations/forms.py
@@ -3,13 +3,13 @@ from django import forms
 from django.core.exceptions import ValidationError
 
 class MoneyDonationForm(forms.Form):
-    money_total = forms.DecimalField(label='Money Total (USD)', max_digits=8, decimal_places=2, min_value=0.01)
+    money_total = forms.DecimalField(label='Money Total (USD)', max_digits=8, decimal_places=2, min_value=0)
 
 class TimeDonationForm(forms.Form):
     time_total = forms.DurationField(label='Time Total (s)')
 
     def clean_time_total(self):
-    	data = self.cleaned_data['time_total']
-    	if data <= timedelta(0):
-    		raise ValidationError("Your volunteer time must be positive")
-    	return data
+        data = self.cleaned_data['time_total']
+        if data <= timedelta(0):
+            raise ValidationError("Your volunteer time must be positive")
+        return data
--- donations/tests.py
+++ donations/tests.py
@@ -1,9 +1,43 @@
+from datetime import timedelta
 from django.contrib.auth.models import User
 from django.test import TestCase
 
 from .forms import MoneyDonationForm, TimeDonationForm
 
 class MoneyDonationFormTests(TestCase):
-	def test_equivalence(self):
-		form = MoneyDonationForm(data={'money_total': 2})
-		self.assertTrue(form.is_valid())
+    def test_equivalence(self):
+        form = MoneyDonationForm(data={'money_total': 1.00})
+        self.assertTrue(form.is_valid())
+
+    def test_boundary1(self):
+        form = MoneyDonationForm(data={'money_total': 0.01})
+        self.assertTrue(form.is_valid())
+
+    def test_boundary2(self):
+        form = MoneyDonationForm(data={'money_total': 999999.99})
+        self.assertTrue(form.is_valid())
+
+    def test_exception1(self):
+        form = MoneyDonationForm(data={'money_total': 0.00})
+        self.assertFalse(form.is_valid())
+
+    def test_exception2(self):
+        form = MoneyDonationForm(data={'money_total': 1000000.00})
+        self.assertFalse(form.is_valid())
+
+class TimeDonationFormTests(TestCase):
+    def test_equivalence(self):
+        form = TimeDonationForm(data={'time_total': timedelta(hours=1)})
+        self.assertTrue(form.is_valid())
+
+    def test_boundary1(self):
+        form = TimeDonationForm(data={'time_total': timedelta(seconds=1)})
+        self.assertTrue(form.is_valid())
+
+    def test_boundary2(self):
+        form = TimeDonationForm(data={'time_total': timedelta.max})
+        self.assertTrue(form.is_valid())
+
+    def test_exception(self):
+        form = TimeDonationForm(data={'time_total': timedelta(seconds=-1)})
+        self.assertFalse(form.is_valid())
