974907ebc3be03040b5ebf8372aac2aaa4357a19
Eric Thomas <ewolft@gmail.com>
2020-11-05 20:21:24 -0500

    Add splits to admin site

--- donations/admin.py
+++ donations/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from .models import MoneyDonation, TimeDonation
+from .models import MoneyDonation, TimeDonation, MoneySplit, TimeSplit
 
 admin.site.register(MoneyDonation)
 admin.site.register(TimeDonation)
