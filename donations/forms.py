3727f7b912fbd8fc2a608f76f0e4319cbb7295c5
Eric Thomas <ewolft@gmail.com>
2020-11-12 18:32:52 -0500

    Try to make the sliders work

--- donations/forms.py
+++ donations/forms.py
@@ -4,18 +4,18 @@ from django import forms
 from django.core.exceptions import ValidationError
 
 CHARITY_CHOICES = (
-    ("1", "The Trevor Project"),
-    ("2", "The National Immigration Law Center"),
-    ("3", "Human Rights Watch"),
-    ("4", "The Global Fund for Women"),
-    ("5", "Charity Water"),
-    ("6", "Mental Health America"),
+    ("The Trevor Project", "The Trevor Project"),
+    ("The National Immigration Law Center", "The National Immigration Law Center"),
+    ("Human Rights Watch", "Human Rights Watch"),
+    ("The Global Fund for Women", "The Global Fund for Women"),
+    ("Charity Water", "Charity Water"),
+    ("Mental Health America", "Mental Health America"),
 )
 
 class MoneyDonationForm(forms.Form):
     money_total = forms.DecimalField(label='Money Total (USD)', max_digits=8, decimal_places=2, min_value=Decimal('0.01'))
     money_splits = forms.CharField(label='Donation Splits (Enter decimal fractions separated by ,)')
-    charities = forms.MultipleChoiceField(label='Charities (Enter charities separated by ,)', choices=CHARITY_CHOICES)
+    charities = forms.MultipleChoiceField(label='Charities (Hold Ctrl to select multiple)', choices=CHARITY_CHOICES)
 
 class TimeDonationForm(forms.Form):
     time_total = forms.DurationField(label='Time Total (s)')
