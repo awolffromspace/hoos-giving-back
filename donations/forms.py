87538b77e434253ae989657d2829b88aba3e9259
Eric Thomas <ewolft@gmail.com>
2020-11-13 14:57:21 -0500

    Testing

--- donations/forms.py
+++ donations/forms.py
@@ -4,7 +4,7 @@ from django import forms
 from django.core.exceptions import ValidationError
 
 class MoneyDonationForm(forms.Form):
-    money_total = forms.DecimalField(label='Donation Total', max_digits=14, decimal_places=2, min_value=Decimal('0.01'), widget=forms.HiddenInput())
+    money_total = forms.DecimalField(label='Donation Total', max_digits=14, decimal_places=2, min_value=Decimal('0.01'))
     money_splits = forms.CharField(label='Donation Splits', widget=forms.HiddenInput())
 
 class TimeDonationForm(forms.Form):
