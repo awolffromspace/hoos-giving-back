9820733757aca4db7c55ac045eec88c555f822f3
Eric Thomas <ewolft@gmail.com>
2020-11-17 13:21:20 -0500

    Implement logout

--- google_login/urls.py
+++ google_login/urls.py
@@ -4,5 +4,6 @@ from . import views
 
 app_name = 'google_login'
 urlpatterns = [
+    path('', TemplateView.as_view(template_name="google_login/index.html")),
     path('logout/', views.logout_request, name='logout'),
 ]
