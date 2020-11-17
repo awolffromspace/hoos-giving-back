cec8366bc90486a9ba647a8c49138cde95270026
Eric Thomas <ewolft@gmail.com>
2020-11-17 13:31:15 -0500

    Implement logout

--- google_login/views.py
+++ google_login/views.py
@@ -3,5 +3,3 @@ from django.shortcuts import render
 
 def logout_request(request):
 	logout(request)
-	messages.info(request, "Logged out successfully!")
-	return redirect("https://project-1-34.herokuapp.com/")
