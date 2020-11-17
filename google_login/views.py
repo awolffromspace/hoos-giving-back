96e0659cf6f265871345cffc7aef6ca8e42643ff
Eric Thomas <ewolft@gmail.com>
2020-11-17 13:53:31 -0500

    Implement logout

--- google_login/views.py
+++ google_login/views.py
@@ -1,5 +1,6 @@
 from django.contrib.auth import logout
 from django.shortcuts import redirect, render
+from django.views.generic import TemplateView
 
 def logout_request(request):
     logout(request)
