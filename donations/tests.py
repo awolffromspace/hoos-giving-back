9edadf42612d9466ddcf10b8d7bdbc8ef8980111
Eric Thomas <ewolft@gmail.com>
2020-11-23 16:28:59 -0500

    Update test cases

--- donations/tests.py
+++ donations/tests.py
@@ -1,5 +1,3 @@
-from datetime import timedelta
-from decimal import *
 from django.contrib.auth.models import User
 from django.test import TestCase
 
@@ -39,45 +37,45 @@ class TaskFormTests(TestCase):
 
 class process_splitsTests(TestCase):
     def test_equivalence1(self):
-        splits = views.process_splits('1.00')
+        splits, sum = views.process_splits('1.00')
         self.assertFalse(splits[0] > 1.00 or splits[0] < 1.00)
 
     def test_equivalence2(self):
-        splits = views.process_splits('1.00,2.00')
+        splits, sum = views.process_splits('1.00,2.00')
         self.assertFalse(splits[0] > 1.00 or splits[0] < 1.00)
         self.assertFalse(splits[1] > 2.00 or splits[1] < 2.00)
 
     def test_equivalence3(self):
-        splits = views.process_splits('1.00,2.00,3.00')
+        splits, sum = views.process_splits('1.00,2.00,3.00')
         self.assertFalse(splits[0] > 1.00 or splits[0] < 1.00)
         self.assertFalse(splits[1] > 2.00 or splits[1] < 2.00)
         self.assertFalse(splits[2] > 3.00 or splits[2] < 3.00)
 
     def test_boundary1(self):
-        splits = views.process_splits('0.01')
+        splits, sum = views.process_splits('0.01')
         self.assertFalse(splits[0] > 0.01 or splits[0] < 0.01)
 
     def test_boundary2(self):
-        splits = views.process_splits('999999999.99')
+        splits, sum = views.process_splits('999999999.99')
         self.assertFalse(splits[0] > 999999999.99 or splits[0] < 999999999.99)
 
     def test_boundary3(self):
-        splits = views.process_splits('0.01,999999999.98')
+        splits, sum = views.process_splits('0.01,999999999.98')
         self.assertFalse(splits[0] > 0.01 or splits[0] < 0.01)
         self.assertFalse(splits[1] > 999999999.98 or splits[1] < 999999999.98)
 
     def test_exception1(self):
-        splits = views.process_splits('0.00')
+        splits, sum = views.process_splits('0.00')
         self.assertTrue(splits[0] == -1)
 
     def test_exception2(self):
-        splits = views.process_splits('1000000000.00')
+        splits, sum = views.process_splits('1000000000.00')
         self.assertTrue(splits[0] == -1)
 
     def test_exception3(self):
-        splits = views.process_splits('-1.00')
+        splits, sum = views.process_splits('-1.00')
         self.assertTrue(splits[0] == -1)
 
     def test_exception4(self):
-        splits = views.process_splits('text')
+        splits, sum = views.process_splits('text')
         self.assertTrue(splits[0] == -1)
