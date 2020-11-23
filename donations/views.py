ca3e04c4bdd571eadeee4c4734e2054f6fec4a17
Eric Thomas <ewolft@gmail.com>
2020-11-23 16:32:08 -0500

    Update test cases

--- donations/views.py
+++ donations/views.py
@@ -60,7 +60,7 @@ def process_splits(splits_str):
         except ValueError:
             return [-1], sum
     if sum < 0.01 or sum > 999999999.99:
-        return [-1]
+        return [-1], sum
     return splits, sum
 
 def update_level(user):
